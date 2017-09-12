2bd380e5224cb55980bad440cbc6d1589da2fd92
AntTv <epicfire95@gmail.com>
2017-09-12 11:44:14 -0400

    Update test.lua

--- test.lua
+++ test.lua
@@ -8,3 +8,17 @@ end
  
  
  
+else if
+Workspace.Object.color3.Value == [255,255,255] then
+ space.FilteringEnabled = false
+ end
+  end
+
+else if 
+Workspace.Object.Color3.Value == [15,15,15] then
+space.FilteringEnabled = true
+space.End()
+end
+end
+ 
+ 
