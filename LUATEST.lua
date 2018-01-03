ee227b30e67e0c47759710ec9d9a83e3bbbba37b
AntTv <epicfire95@gmail.com>
2018-01-03 09:28:17 -0500

    Create LUATEST.lua

---
+++ LUATEST.lua
@@ -0,0 +1,14 @@
+local Object = newInstance("Part")
+local MValue = newInstant("FileMesh")
+local Workspace = Workspace
+local assetid = "105258827"
+
+Object.Parent = Workspace
+Object.Position = ("-155.210","121.157","-119.524")
+MValue.Parent = Workspace.Object
+MValue.AssetID = "assetid://" ...assetid
+       end
+    end
+ end
+--X-Y-Z-GRIDDING--
+--APPLYING MESH VALUE INTO AN OBJECT
